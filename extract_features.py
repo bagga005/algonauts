43bc6569e6e97904b2883f0bc5e0334b15e2a3b3
Kunal Bagga <kunal@kunalb.com>
2025-02-12 15:05:45 +0000

    extraction

--- extract_features.py
+++ extract_features.py
@@ -7,9 +7,9 @@ def extract_raw_visual_features():
     root_data_dir = utils.get_data_root_dir()
     out_data_dir = utils.get_output_dir()
 # As an exemple, extract visual features for season 1, episode 1 of Friends
-    episode_path = root_data_dir + "algonauts_2025.competitors/stimuli/movies/friends/s1/friends_s01e01a.mkv"
+    #episode_path = root_data_dir + "algonauts_2025.competitors/stimuli/movies/friends/s1/friends_s01e01a.mkv"
     # Collecting the paths to all the movie stimuli
-    files = glob(f"{root_data_dir}/algonauts_2025.competitors/stimuli/movies/**/**/*.mkv")
+    files = glob(f"{root_data_dir}algonauts_2025.competitors/stimuli/movies/**/**/*.mkv")
     files.sort()
     stimuli = {f.split("/")[-1].split(".")[0]: f for f in files}
     print(len(stimuli), list(stimuli)[:3], list(stimuli)[-3:])
@@ -27,10 +27,9 @@ def extract_raw_visual_features():
     for i, (stim_id, stim_path) in iterator:
         print(f"Extracting visual features for {stim_id}", stim_path)
         fn = f"{out_data_dir}/stim_id.h5"
-    # Execute visual feature extraction
-<<<<<<< HEAD
-        visual_features = extract_visual_features(episode_path, tr, feature_extractor,
-        model_layer, transform, device, save_dir_temp, save_dir_features)
+        # Execute visual feature extraction
+        visual_features = extract_visual_features(stim_path, tr, feature_extractor,
+        model_layer, transform, device, save_dir_temp, fn, stim_id)
 
 if __name__ == "__main__":
     extract_raw_visual_features()
