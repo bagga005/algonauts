3c3fc8ea45d89a8b203a18bb41634272e4075169
Kunal Bagga <kunal@kunalb.com>
2025-05-18 22:54:53 +0100

    add video for internvl

--- model_intervl.py
+++ model_intervl.py
@@ -88,6 +88,38 @@ def load_image(image_file, input_size=448, max_num=12):
     pixel_values = torch.stack(pixel_values)
     return pixel_values
 
+def get_index(bound, fps, max_frame, first_idx=0, num_segments=32):
+    if bound:
+        start, end = bound[0], bound[1]
+    else:
+        start, end = -100000, 100000
+    start_idx = max(first_idx, round(start * fps))
+    end_idx = min(round(end * fps), max_frame)
+    seg_size = float(end_idx - start_idx) / num_segments
+    frame_indices = np.array([
+        int(start_idx + (seg_size / 2) + np.round(seg_size * idx))
+        for idx in range(num_segments)
+    ])
+    return frame_indices
+
+def load_video(video_path, bound=None, input_size=448, max_num=1, num_segments=32):
+    vr = VideoReader(video_path, ctx=cpu(0), num_threads=1)
+    max_frame = len(vr) - 1
+    fps = float(vr.get_avg_fps())
+
+    pixel_values_list, num_patches_list = [], []
+    transform = build_transform(input_size=input_size)
+    frame_indices = get_index(bound, fps, max_frame, first_idx=0, num_segments=num_segments)
+    for frame_index in frame_indices:
+        img = Image.fromarray(vr[frame_index].asnumpy()).convert('RGB')
+        img = dynamic_preprocess(img, image_size=input_size, use_thumbnail=True, max_num=max_num)
+        pixel_values = [transform(tile) for tile in img]
+        pixel_values = torch.stack(pixel_values)
+        num_patches_list.append(pixel_values.shape[0])
+        pixel_values_list.append(pixel_values)
+    pixel_values = torch.cat(pixel_values_list)
+    return pixel_values, num_patches_list
+
 def split_model(model_name):
     device_map = {}
     world_size = torch.cuda.device_count()
@@ -509,14 +541,23 @@ model = AutoModel.from_pretrained(
     device_map=device_map).eval()
 tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True, use_fast=False)
 
+#process Image
 # set the max number of tiles in `max_num`
-pixel_values = load_image('https://techcrunch.com/wp-content/uploads/2025/02/GettyImages-2197091379.jpg', max_num=12).to(torch.bfloat16).cuda()
+# pixel_values = load_image('https://techcrunch.com/wp-content/uploads/2025/02/GettyImages-2197091379.jpg', max_num=12).to(torch.bfloat16).cuda()
+# question_for_embeddings = '<image>\nSam Altman is the CEO of OpenAI. He is known for his ability to deal make and raise near endless money for OpenAI.'
 generation_config = dict(max_new_tokens=1024, do_sample=True)
 
-
+#process video
+video_path = 'temp/red-panda.mp4'
+pixel_values, num_patches_list = load_video(video_path, num_segments=7, max_num=1)
+pixel_values = pixel_values.to(torch.bfloat16).cuda()
+video_prefix = ''.join([f'Frame{i+1}: <image>\n' for i in range(len(num_patches_list))])
+question_for_embeddings = video_prefix + 'What is the red panda doing?'
+# Frame1: <image>\nFrame2: <image>\n...\nFrame8: <image>\n{question}
+print('question_for_embeddings:', question_for_embeddings)
 
 # Example of extracting embeddings using hooks
-question_for_embeddings = '<image>\nSam Altman is the CEO of OpenAI. He is known for his ability to deal make and raise near endless money for OpenAI.'
+
 layer_names = [
     'vision_model',  # Vision encoder embeddings
     'language_model.model.layers.0',  # First LLM layer
@@ -539,7 +580,7 @@ embeddings = get_layer_by_layer_embeddings(
     model, 
     tokenizer, 
     pixel_values, 
-    "<image>\nDescribe what you see in this image.",
+    question_for_embeddings,
     custom_layers
 )
 
