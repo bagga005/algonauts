00d268188d46df6e1b2c6acae73384a0bd678559
Kunal Bagga <kunal@kunalb.com>
2025-06-14 22:28:37 +0100

    -

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import h5py
 import os
 import utils
+from tqdm import tqdm
 from model_sklearn import LinearHandler_Sklearn
 from model_torchregression import RegressionHander_Pytorch
 from model_transformer import RegressionHander_Transformer
@@ -182,6 +183,184 @@ def normalize_to_radians(value, original_min=0, original_max=49, target_min=-mat
     scaled = normalized * (target_max - target_min) + target_min
     
     return scaled
+
+def load_stimulus_features_friends_s7(root_data_dir):
+    """
+    Load the stimulus features of all modalities (visual + audio + language) for
+    Friends season 7.
+
+    Parameters
+    ----------
+    root_data_dir : str
+        Root data directory.
+
+    Returns
+    -------
+    features_friends_s7 : dict
+        Dictionary containing the stimulus features for Friends season 7.
+
+    """
+
+    features_friends_s7 = {}
+
+    ### Load the visual features ###
+    stimuli_dir = os.path.join(root_data_dir, 'stimulus_features', 'pca',
+        'friends_movie10', 'visual', 'features_test.npy')
+    features_friends_s7['visual'] = np.load(stimuli_dir,
+        allow_pickle=True).item()
+
+    ### Load the audio features ###
+    stimuli_dir = os.path.join(root_data_dir, 'stimulus_features', 'pca',
+        'friends_movie10', 'audio', 'features_test.npy')
+    features_friends_s7['audio'] = np.load(stimuli_dir,
+        allow_pickle=True).item()
+
+    ### Load the language features ###
+    stimuli_dir = os.path.join(root_data_dir, 'stimulus_features', 'pca',
+        'friends_movie10', 'language', 'features_test.npy')
+    features_friends_s7['language'] = np.load(stimuli_dir,
+        allow_pickle=True).item()
+
+    ### Output ###
+    return features_friends_s7
+
+def align_features_and_fmri_samples_friends_s7(features_friends_s7,
+    root_data_dir):
+    """
+    Align the stimulus feature with the fMRI response samples for Friends season
+    7 episodes, later used to predict the fMRI responses for challenge
+    submission.
+
+    Parameters
+    ----------
+    features_friends_s7 : dict
+        Dictionary containing the stimulus features for Friends season 7.
+    root_data_dir : str
+        Root data directory.
+
+    Returns
+    -------
+    aligned_features_friends_s7 : dict
+        Aligned stimulus features for each subject and Friends season 7 episode.
+
+    """
+
+    ### Empty results dictionary ###
+    aligned_features_friends_s7 = {}
+
+    ### HRF delay ###
+    # fMRI detects the BOLD (Blood Oxygen Level Dependent) response, a signal
+    # that reflects changes in blood oxygenation levels in response to activity
+    # in the brain. Blood flow increases to a given brain region in response to
+    # its activity. This vascular response, which follows the hemodynamic
+    # response function (HRF), takes time. Typically, the HRF peaks around 5–6
+    # seconds after a neural event: this delay reflects the time needed for
+    # blood oxygenation changes to propagate and for the fMRI signal to capture
+    # them. Therefore, this parameter introduces a delay between stimulus chunks
+    # and fMRI samples for a better correspondence between input stimuli and the
+    # brain response. For example, with a hrf_delay of 3, if the stimulus chunk
+    # of interest is 17, the corresponding fMRI sample will be 20.
+    hrf_delay = 3
+
+    ### Stimulus window ###
+    # stimulus_window indicates how many stimulus feature samples are used to
+    # model each fMRI sample, starting from the stimulus sample corresponding to
+    # the fMRI sample of interest, minus the hrf_delay, and going back in time.
+    # For example, with a stimulus_window of 5, and a hrf_delay of 3, if the
+    # fMRI sample of interest is 20, it will be modeled with stimulus samples
+    # [13, 14, 15, 16, 17]. Note that this only applies to visual and audio
+    # features, since the language features were already extracted using
+    # transcript words spanning several movie samples (thus, each fMRI sample
+    # will only be modeled using the corresponding language feature sample,
+    # minus the hrf_delay). Also note that a larger stimulus window will
+    # increase compute time, since it increases the amount of stimulus features
+    # used to train and validate the fMRI encoding models. Here you will use a
+    # value of 5, since this is how the challenge baseline encoding models were
+    # trained.
+    stimulus_window = 5
+
+    ### Loop over subjects ###
+    subjects = [1, 2, 3, 5]
+    desc = "Aligning stimulus and fMRI features of the four subjects"
+    for sub in tqdm(subjects, desc=desc):
+        aligned_features_friends_s7[f'sub-0{sub}'] = {}
+
+        ### Load the Friends season 7 fMRI samples ###
+        samples_dir = os.path.join(root_data_dir, 'algonauts_2025.competitors',
+            'fmri', f'sub-0{sub}', 'target_sample_number',
+            f'sub-0{sub}_friends-s7_fmri_samples.npy')
+        fmri_samples = np.load(samples_dir, allow_pickle=True).item()
+
+        ### Loop over Friends season 7 episodes ###
+        for epi, samples in fmri_samples.items():
+            features_epi = []
+
+            ### Loop over fMRI samples ###
+            for s in range(samples):
+                # Empty variable containing the stimulus features of all
+                # modalities for each sample
+                f_all = np.empty(0)
+
+                ### Loop across modalities ###
+                for mod in features_friends_s7.keys():
+
+                    ### Visual and audio features ###
+                    # If visual or audio modality, model each fMRI sample using
+                    # the N stimulus feature samples up to the fMRI sample of
+                    # interest minus the hrf_delay (where N is defined by the
+                    # 'stimulus_window' variable)
+                    if mod == 'visual' or mod == 'audio':
+                        # In case there are not N stimulus feature samples up to
+                        # the fMRI sample of interest minus the hrf_delay (where
+                        # N is defined by the 'stimulus_window' variable), model
+                        # the fMRI sample using the first N stimulus feature
+                        # samples
+                        if s < (stimulus_window + hrf_delay):
+                            idx_start = 0
+                            idx_end = idx_start + stimulus_window
+                        else:
+                            idx_start = s - hrf_delay - stimulus_window + 1
+                            idx_end = idx_start + stimulus_window
+                        # In case there are less visual/audio feature samples
+                        # than fMRI samples minus the hrf_delay, use the last N
+                        # visual/audio feature samples available (where N is
+                        # defined by the 'stimulus_window' variable)
+                        if idx_end > len(features_friends_s7[mod][epi]):
+                            idx_end = len(features_friends_s7[mod][epi])
+                            idx_start = idx_end - stimulus_window
+                        f = features_friends_s7[mod][epi][idx_start:idx_end]
+                        f_all = np.append(f_all, f.flatten())
+
+                    ### Language features ###
+                    # Since language features already consist of embeddings
+                    # spanning several samples, only model each fMRI sample
+                    # using the corresponding stimulus feature sample minus the
+                    # hrf_delay
+                    elif mod == 'language':
+                        # In case there are no language features for the fMRI
+                        # sample of interest minus the hrf_delay, model the fMRI
+                        # sample using the first language feature sample
+                        if s < hrf_delay:
+                            idx = 0
+                        else:
+                            idx = s - hrf_delay
+                        # In case there are fewer language feature samples than
+                        # fMRI samples minus the hrf_delay, use the last
+                        # language feature sample available
+                        if idx >= (len(features_friends_s7[mod][epi]) - hrf_delay):
+                            f = features_friends_s7[mod][epi][-1,:]
+                        else:
+                            f = features_friends_s7[mod][epi][idx]
+                        f_all = np.append(f_all, f.flatten())
+
+                ### Append the stimulus features of all modalities for this sample ###
+                features_epi.append(f_all)
+
+            ### Add the episode stimulus features to the features dictionary ###
+            aligned_features_friends_s7[f'sub-0{sub}'][epi] = np.asarray(
+                features_epi, dtype=np.float32)
+
+    return aligned_features_friends_s7
 
 def align_features_and_fmri_samples(features, fmri, excluded_samples_start,
     excluded_samples_end, hrf_delay, stimulus_window, movies, viewing_session, summary_features=False, all_subject_fmri=False):
@@ -830,6 +1009,8 @@ def run_validation(subject, modality, features, fmri, excluded_samples_start, ex
         features_val, fmri_val = align_features_and_fmri_samples(features, fmri,
         excluded_samples_start, excluded_samples_end, hrf_delay, stimulus_window,
         movies_val, viewing_session)
+        print('features_val.shape', features_val.shape)
+        print('fmri_val.shape', fmri_val.shape)
         trainer = LinearHandler_Sklearn(features_val.shape[1], fmri_val.shape[1])
     elif training_handler == 'transformer':
          # Align the stimulus features with the fMRI responses for the validation movies
