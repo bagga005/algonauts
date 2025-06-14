4b13a8851ba186ebb49fe526666918583ea1886a
Kunal Bagga <kunal@kunalb.com>
2025-06-14 14:37:48 +0100

    -

--- extract_features.py
+++ extract_features.py
@@ -121,8 +121,7 @@ def extract_preprocessed_video_content():
     # Collecting the paths to all the movie stimuli
     file_in_filter = utils.get_stimuli_prefix()
     exclude_list = []#['friends_s03e05b', 'friends_s03e06a']
-    files = glob(f"{root_data_dir}/algonauts_2025.competitors/stimuli/movies/movie10/**/*.mkv")
-    print(len(files))
+    files = glob(f"{root_data_dir}/algonauts_2025.competitors/stimuli/movies/*/**/*.mkv")
     if file_in_filter:
         # Support comma-separated patterns
         filters = file_in_filter.split(',')
