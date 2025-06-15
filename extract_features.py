a74676e0a4caf9708758a1df4bfc65f07ced55d8
Kunal Bagga <kunal@kunalb.com>
2025-06-15 12:05:16 +0100

    s7

--- extract_features.py
+++ extract_features.py
@@ -894,7 +894,7 @@ if __name__ == "__main__":
     embeddings_combined_dir = utils.get_embeddings_combined_dir()
     dir_output_path = os.path.join(out_dir, embeddings_combined_dir)
 
-    filter_in_name = ["s01", "s02", "s03", "s04", "s05", "s06"]
+    filter_in_name = ["s01", "s02", "s03", "s04", "s05", "s06", "s07"]
     # filter_in_name = [ "s02","s03", "s04",  "s06"]
     #filter_in_name = ["s03", "s04", "s05", "s06"]
     modality = "visual"
