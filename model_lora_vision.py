ebf837407d78f0909cd82ce5cad8e45f7c4228fc
Kunal Bagga <kunal@kunalb.com>
2025-06-17 23:16:44 +0100

    -

--- model_lora_vision.py
+++ model_lora_vision.py
@@ -152,7 +152,7 @@ class VideoDataset(torch.utils.data.Dataset):
     
     def __getitem__(self, idx):
         videoname, frame_indices = self.input_data[idx]
-        print(f'videoname: {videoname}', f'frame_indices: {frame_indices}', f'idx: {idx}')
+        #print(f'videoname: {videoname}', f'frame_indices: {frame_indices}', f'idx: {idx}')
         filename = os.path.join(utils.get_stimulus_pre_features_dir(), 'pre', 'visual', videoname+'.h5')
         return videoname, frame_indices, idx
         if utils.isMockMode():
