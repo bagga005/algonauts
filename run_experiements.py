ba6ea1555fea0adf838727766e14812a9f397593
Kunal Bagga <kunal@kunalb.com>
2025-06-17 13:43:42 +0100

    -

--- run_experiements.py
+++ run_experiements.py
@@ -20,7 +20,7 @@ def run_trainings(experiment_name=None, results_output_directory=None):
     subject = 1
     include_viewing_sessions = False
     skip_accuracy_check = False
-    movies_train = ["friends-s01", "friends-s02", "friends-s03", "friends-s04", "friends-s05"] #, "movie10-bourne",  "movie10-wolf", "movies10-life"] # @param {allow-input: true}
+    movies_train = ["friends-s01", "friends-s02", "friends-s03", "friends-s04", "friends-s05"]#, "friends-s06", "movie10-bourne",  "movie10-wolf", "movies10-figures"] #, "movie10-bourne",  "movie10-wolf", "movies10-life"] # @param {allow-input: true}
     #movies_train = ["friends-s01"] # @param {allow-input: true}
     movies_val = ["friends-s06"] # @param {allow-input: true}
     
