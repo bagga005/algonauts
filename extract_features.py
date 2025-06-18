b2b29269440483818aa1be430e242a847cc2b189
Kunal Bagga <kunal@kunalb.com>
2025-06-18 01:30:14 +0100

    -

--- extract_features.py
+++ extract_features.py
@@ -932,7 +932,7 @@ if __name__ == "__main__":
                 strategy = arg
                 strategy_id = globals()[strategy]
                 kwargs = dict(modality=modality, filter_in_name=filter_in_name, \
-                    #overwrite_pca=True, \
+                    overwrite_pca=True, \
                     #overwrite=True, \
                     #pca_skip=True \
                     # force_evaluation=True \
