dc284b0bc4835bbae711f9a6aec73b52c7fe3ff2
Kunal Bagga <kunal@kunalb.com>
2025-06-18 13:13:15 +0100

    -

--- extract_features.py
+++ extract_features.py
@@ -973,7 +973,7 @@ if __name__ == "__main__":
     #filter_in_name = ["s01", "s02", "s03", "s04", "s05", "s06", "s07"]
     # filter_in_name = [ "s02","s03", "s04",  "s06"]
     #filter_in_name = ["s03"]
-    filter_in_name = ["life", "s01", "s02", "s03", "s04", "s05"]
+    filter_in_name = ["life", "s01", "s02", "s03", "s04", "s05", "s06"]
     modality = "visual"
     
     strategy ="STRATEGY_V4_POST_L12_L10_AVG"
