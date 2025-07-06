203a3fe90fadd114aa2e4b61277804781d5b136e
Kunal Bagga <kunal@kunalb.com>
2025-07-06 16:42:55 +0100

    -

--- _temp_renamefiles.py
+++ _temp_renamefiles.py
@@ -1,12 +1,23 @@
 import os
+import sys
 
-# Get all files in the current directory
-for filename in os.listdir('.'):
-    # Only process files (not directories)
-    if os.path.isfile(filename):
-        # Remove 'task-' and 'video' from filename
+if len(sys.argv) != 2:
+    print("Usage: python rename_files.py /path/to/directory")
+    sys.exit(1)
+
+directory = sys.argv[1]
+
+# Check if the provided path is a directory
+if not os.path.isdir(directory):
+    print(f"Error: '{directory}' is not a directory.")
+    sys.exit(1)
+
+# Process files in the given directory
+for filename in os.listdir(directory):
+    full_path = os.path.join(directory, filename)
+    if os.path.isfile(full_path):
         new_name = filename.replace('task-', '').replace('video', '')
-        # Only rename if the name actually changes
         if new_name != filename:
-            os.rename(filename, new_name)
+            new_path = os.path.join(directory, new_name)
+            os.rename(full_path, new_path)
             print(f"Renamed: {filename} -> {new_name}")
