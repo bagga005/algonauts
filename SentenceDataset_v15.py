60339dd5d8babe9cdb77a2dd4afd4e8f2c40337f
Kunal Bagga <kunal@kunalb.com>
2025-07-07 19:39:00 +0100

    -

--- SentenceDataset_v15.py
+++ SentenceDataset_v15.py
@@ -100,11 +100,7 @@ def load_all_tsv_for_one_episode(stim_id, isEnhanced=False):
     if isEnhanced:
         t_files = glob(f"{root_data_dir}/algonauts_2025.competitors/stimuli/transcripts/friends/s*/enhanced/{stim_id}*.tsv")
     else:
-        if 'friends' not in stim_id:
-            print('not friends ', stim_id)
-            t_files = glob(f"{root_data_dir}/algonauts_2025.competitors/stimuli/transcripts/movie10/pulpfiction/{stim_id}*.tsv")
-        else:
-            t_files = glob(f"{root_data_dir}/algonauts_2025.competitors/stimuli/transcripts/friends/*/{stim_id}*.tsv")
+        t_files = glob(f"{root_data_dir}/algonauts_2025.competitors/stimuli/transcripts/*/*/{stim_id}*.tsv")
     t_files.sort()
     f_stimuli = {f.split("/")[-1].split(".")[0]: f for f in t_files}
     print(len(f_stimuli), list(f_stimuli)[:3], list(f_stimuli)[-3:])
@@ -134,12 +130,13 @@ def load_all_tsv_for_one_episode(stim_id, isEnhanced=False):
 
 def get_full_transcript(stim_id):
     print(f"Getting full transcript for {stim_id}")
+    if 'friends' not in stim_id:
+        stim_id = 'movie10_' + stim_id 
     stim_iden = stim_id[:-1]
     if stim_iden[-1] == '0' or stim_iden[-1] == '1':
         stim_iden = stim_iden[:-1]
-    if 'friends' not in stim_iden:
-        stim_iden = 'movie10_' + stim_iden 
-    print(stim_iden)
+
+    print(stim_iden, stim_id)
     transcript_data, trans_info_list, total_tr_len = load_all_tsv_for_one_episode(stim_iden, isEnhanced=False)
     tr_start = 0
     tr_length =0
