ac60aed66e4010c24422491e4c717b529dae9ccf
Kunal Bagga <kunal@kunalb.com>
2025-07-08 09:45:37 +0100

    -

--- extract_features.py
+++ extract_features.py
@@ -971,7 +971,10 @@ if __name__ == "__main__":
     # filter_in_name = [ "s02","s03", "s04",  "s06"]
     #filter_in_name = ["s03"]
     #filter_in_name = ["life", "s01", "s02", "s03", "s04", "s05", "s06", "s07", "bourne", "figures", "wolf"]
-    filter_in_name = ["life","bourne","figures","s01", "s02", "s03", "s04", "s05", "s06", "s07","wolf","wot","pulpfiction","chaplin","passepartout","mononoke"]
+    prefix_files = utils.get_stimuli_prefix
+    assert len(prefix_files) > 0, "No prefix files found"
+    filter_in_name = prefix_files.split(',') #["life","bourne","figures","s01", "s02", "s03", "s04", "s05", "s06", "s07","wolf","wot","pulpfiction","chaplin","passepartout","mononoke"]
+    #s01,s02,s03,s04,s05,s06,s07,life,bourne,figures,wolf,wot,pulpfiction,passepartout,mononoke,planetearth
     modality = "visual"
     
     strategy ="STRATEGY_V4_POST_L12_L10_AVG"
